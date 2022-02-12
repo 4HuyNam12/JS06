a62a7347493e0b07af7091444404caff962abab6
4HuyNam12 <formysadsong412@gmail.com>
2022-02-12 18:24:03 +0700

    them test

--- JSbuoi1/btvn1.js
+++ JSbuoi1/btvn1.js
@@ -5,17 +5,21 @@ function calculateRectangle(length, width) {
   console.log("Diện tích hình chữ nhật là : " + area);
 }
 
+calculateRectangle(5, 4);
+
 function calculateCircle(radius) {
   let perimeter = 2 * 3.14 * radius;
   let area = radius * radius * 3.14;
   console.log("Chu vi hình tròn là : " + perimeter);
   console.log("Diện tích hình tròn là : " + area);
 }
+calculateCircle(3);
 
 function simpleEquation(a, b) {
   let result = -b / a;
   console.log("Phương trình có một nghiệm x =" + result);
 }
+simpleEquation(4, 5);
 
 function unitConversion(number) {
   let mmConvert = number * 10;
@@ -25,10 +29,12 @@ function unitConversion(number) {
   console.log(number + "cm = " + mConvert + "m");
   console.log(number + "cm = " + kmConvert + "km");
 }
+unitConversion(20);
 
 function heatUnitConversion(number) {
   let fConvert = number * 1.8 + 32;
   let kConvert = number + 273.5;
-  console.log(number + "độ C = " + fConvert + "độ F");
-  console.log(number + "cm = " + kConvert + "độ K");
+  console.log(number + "độ C = " + fConvert + "độF");
+  console.log(number + "cm = " + kConvert + "độK");
 }
+heatUnitConversion(17);
